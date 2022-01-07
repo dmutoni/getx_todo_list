941d696d99a6a7b1fa9caab5aed49fd0d4d410b3
Denyse <mdenyse15@gmail.com>
2022-01-07 22:43:34 +0200

    Feat: (Added rendering tasks on the screen functionality)

---
+++ lib/app/modules/home/widgets/task_card.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_instance/src/extension_instance.dart';
+import 'package:get/get_navigation/src/extension_navigation.dart';
+import 'package:getx_todo_list/app/core/utils/extensions.dart';
+import 'package:getx_todo_list/app/data/model/task.dart';
+import 'package:step_progress_indicator/step_progress_indicator.dart';
+
+import 'controller.dart';
+
+class TaskCard extends StatelessWidget {
+  final homeCtrl = Get.find<HomeController>();
+  final Task task;
+
+  TaskCard({Key? key, required this.task}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final color = HexColor.fromHex(task.color);
+    var squareWidth = Get.width - 12.0.wp;
+    return Container(
+      width: squareWidth / 2,
+      height: squareWidth / 2,
+      margin: EdgeInsets.all(3.0.wp),
+      decoration: BoxDecoration(color: Colors.white, boxShadow: [
+        BoxShadow(
+          color: Colors.grey.shade300,
+          blurRadius: 7,
+          offset: const Offset(0, 7),
+        )
+      ]),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        StepProgressIndicator(
+          totalSteps: 100,
+          currentStep: 80,
+          size: 5,
+          padding: 0,
+          selectedGradientColor: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [color.withOpacity(0.5), color]),
+          unselectedGradientColor: const LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Colors.white, Colors.white]),
+        ),
+        Padding(
+          padding: EdgeInsets.all(6.0.wp),
+          child: Icon(
+            IconData(task.icon, fontFamily: 'MaterialIcons'),
+            color: color,
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(6.0.wp),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                task.title,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 12.0.sp,
+                ),
+                overflow: TextOverflow.ellipsis,
+              ),
+              SizedBox(
+                height: 2.0.wp,
+              ),
+              Text('${task.todos?.length ?? 0} Task',
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.grey))
+            ],
+          ),
+        )
+      ]),
+    );
+  }
+}
--- lib/app/modules/home/widgets/view.dart
+++ lib/app/modules/home/widgets/view.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:get/get_state_manager/src/simple/get_view.dart';
+import 'package:getx_todo_list/app/data/model/task.dart';
 import 'package:getx_todo_list/app/modules/home/widgets/add_cart.dart';
 import 'package:getx_todo_list/app/modules/home/widgets/controller.dart';
 import 'package:getx_todo_list/app/core/utils/extensions.dart';
+import 'package:getx_todo_list/app/modules/home/widgets/task_card.dart';
 
 class HomePage extends GetView<HomeController> {
   const HomePage({Key? key}) : super(key: key);
@@ -22,12 +25,23 @@ class HomePage extends GetView<HomeController> {
             ),
           ),
         ),
-        GridView.count(crossAxisCount: 2,
-          shrinkWrap: true,
-          physics: const ClampingScrollPhysics(),
-          children: [
-            AddCard()
-          ],
+        Obx(() =>
+           GridView.count(
+            crossAxisCount: 2,
+            shrinkWrap: true,
+            physics: const ClampingScrollPhysics(),
+            children: [
+              ...controller.tasks
+                  .map((element) => TaskCard(
+                        task: element,
+                      ))
+                  .toList(),
+              TaskCard(
+                  task: const Task(
+                      title: 'title', icon: 0xe59c, color: '#FF2B60E6')),
+              AddCard()
+            ],
+          ),
         )
       ])),
     );
