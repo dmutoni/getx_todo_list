5a679305b221e93ed72d1f2303cca9156d5a1dd7
Denyse <mdenyse15@gmail.com>
2022-01-10 00:14:21 +0200

    Feat: (Added details progress indicator)

--- lib/app/modules/detail/view.dart
+++ lib/app/modules/detail/view.dart
@@ -5,6 +5,7 @@ import 'package:get/get_navigation/src/extension_navigation.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:getx_todo_list/app/core/utils/extensions.dart';
 import 'package:getx_todo_list/app/modules/home/widgets/controller.dart';
+import 'package:step_progress_indicator/step_progress_indicator.dart';
 
 class DetailPage extends StatelessWidget {
   final homeCtrl = Get.find<HomeController>();
@@ -54,14 +55,40 @@ class DetailPage extends StatelessWidget {
         Obx(
           () {
             var totalTodos = homeCtrl.doingTodos.length + homeCtrl.doneTodos.length;
-            return Row(
-              children: [
-                Text('$totalTodos Tasks',
-                style: TextStyle(
-                  fontSize: 12.0.sp,
-                  color: Colors.grey
-                ))
-              ],
+            return Padding(
+              padding: EdgeInsets.only(
+                left: 16.0.wp,
+                top: 3.0.wp,
+                right: 16.0.wp
+              ),
+              child: Row(
+                children: [
+                  Text('$totalTodos Tasks',
+                  style: TextStyle(
+                    fontSize: 12.0.sp,
+                    color: Colors.grey
+                  )),
+                  SizedBox(width: 3.0.wp,),
+                  Expanded(
+                    child: StepProgressIndicator(
+                      totalSteps: totalTodos == 0 ? 1 : totalTodos,
+                      currentStep: homeCtrl.doneTodos.length,
+                      size: 5,
+                      padding: 0,
+                      selectedGradientColor: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [color.withOpacity(0.5), color]
+                      ),
+                      unselectedGradientColor: LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: [Colors.grey[300]!, Colors.grey[300]!]
+                      ),
+                    ),
+                  )
+                ],
+              ),
             );
           }
         )
